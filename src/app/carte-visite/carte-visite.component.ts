4184617937191bcedea33f0e4202e76e9a2b9c70
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-30 00:15:40 +0100

    Gérer la soumission du formulaire

--- src/app/carte-visite/carte-visite.component.ts
+++ src/app/carte-visite/carte-visite.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CarteVisite } from './carte-visite.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carte-visite',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './carte-visite.component.html',
   styleUrl: './carte-visite.component.css'
 })
 export class CarteVisiteComponent {
-
+  carteVisite: CarteVisite = new CarteVisite()
+  onSubmit() {
+    console.log(this.carteVisite)
+  }
 }
