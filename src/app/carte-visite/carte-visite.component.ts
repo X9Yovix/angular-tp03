e9be060b053852bfb682dbc17fee1e533b1c2717
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-29 23:58:24 +0100

    Créer un nouveau composant

---
+++ src/app/carte-visite/carte-visite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carte-visite',
+  standalone: true,
+  imports: [],
+  templateUrl: './carte-visite.component.html',
+  styleUrl: './carte-visite.component.css'
+})
+export class CarteVisiteComponent {
+
+}
